fe2931e62cca1c599188113eccdd261162d8ccfe
hana20210115 <hana20210115@gmail.com>
2026-07-14 15:15:07 +0900

    Create migrations

---
+++ database/migrations/2026_07_14_050019_create_favorites_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('book_id')->constrained();
+            $table->timestamps();
+
+            //複合ユニーク:１ユーザーにつき１冊1レビュー
+            $table->unique(['book_id','user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('favorites');
+    }
+};
